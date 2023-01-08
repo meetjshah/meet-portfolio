934ca9b3abca3f73afbed08281546686cd283b17
meetshah <meetshah@iu.edu>
2023-01-09 01:22:34 +0530

    Tailwind Modifications

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -27,7 +27,7 @@ const ContactMe = (props: Props) => {
         </h3>
 
         <div className='flex flex-col space-y-10'>
-            <h4 className='text-4xl font-semibold text-center'>
+            <h4 className='text-2xl md:text-4xl font-semibold text-center'>
                 {/* I have got just what you need.{" "} */}
                 <span className='decoration-[#F7AB0A]/50 underline'>Let us Talk</span> 
             </h4>
@@ -52,7 +52,7 @@ const ContactMe = (props: Props) => {
 
             <form 
             onSubmit={handleSubmit(onSubmit)}
-            className='flex flex-col space-y-2 w-fit mx-auto'
+            className='flex flex-col space-y-2 w-[300px] md:w-fit mx-auto max-w-sm md:max-w-none'
             >
                 <div className='flex space-x-2'>
                     <input {...register('name')} placeholder='Name' className='contactInput' type='text'/>
