c5b0a14f0a62ebab06eb6ad14c7b42925637c9b2
meetshah <meetshah@iu.edu>
2023-01-09 21:00:45 +0530

    Layout fixes

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -40,13 +40,13 @@ const Skill = ({ titlesrc, proficiency, directionLeft }: Props) => {
             transition={{ duration: 1 }}
             whileInView={{ opacity: 1, x: 0 }}
             src={titlesrc}
-            className='rounded-full border border-gray-500 object-cover w-12 h-12 md:w-24 md:h-24 
+            className='rounded-full border border-gray-500 object-cover w-8 h-8 md:w-24 md:h-24 
             filter group-hover:grayscale transition duration-300 ease-in-out'
         />
-        <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-12
-        w-12 md:w-24 md:h-24 rounded-full z-0'>
+        <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-8
+        w-8 md:w-24 md:h-24 rounded-full z-0'>
             <div className='flex items-center justify-center h-full'>
-                <p className='text-md md:text-3xl font-bold text-black opacity-100'>{proficiency}%</p>
+                <p className='text-xs md:text-3xl font-bold text-black opacity-100'>{proficiency}%</p>
             </div>
         </div>
 
